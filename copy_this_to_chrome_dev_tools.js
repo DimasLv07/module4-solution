b8c512877d3f46f6773b7d84c76d6a553b090638
Dimas Fendriansyah <dimasfendriansyah123098@gmail.com>
2022-01-23 11:31:58 +0700

    first commit

---
+++ copy_this_to_chrome_dev_tools.js
@@ -0,0 +1,41 @@
+(function (window) {
+    var speakWord = "Hello";
+
+    var helloSpeaker = {
+        speak: function (name) {
+            console.log(speakWord + " " + name);
+        }
+    };
+
+    window.helloSpeaker = helloSpeaker
+
+})(window);
+
+
+(function (window) {
+
+    var speakWord = "Good Bye";
+
+    var byeSpeaker = {
+        speak: function (name) {
+            console.log(speakWord + " " + name);
+        }
+    };
+
+
+    window.byeSpeaker = byeSpeaker;
+
+})(window);
+
+
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    if (firstLetter === "j") {
+        byeSpeaker.speak(names[i]);
+    } else {
+        helloSpeaker.speak(names[i]);
+    }
+}
